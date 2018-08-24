ff2ae12185b1ddac860b8764c567deb540419250
michalber <37382499+michalber@users.noreply.github.com>
2018-08-24 19:47:13 +0200

    Update2

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,24 @@ $(function(){
 });
 //--------------------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------------------
+$(document).ready(function(){
+    $(window).scroll(function() { 
+        if ($(document).scrollTop() > 100) { 
+            $("nav").css("background", "rgb(125, 106, 96,0.8)");
+            $("nav ul li a").css("color","#fff");
+            $(".nav-btn i").css("background","#fff");
+            $(".nav-wrapper").css("background","rgb(125, 106, 96)");
+            $(".hover-eff").addClass("hover-color");
+            document.getElementById('logo_black').src='assets/img/logoAP_white.png';
+        } 
+        else {
+            $("nav").css("background", "rgba(255, 255, 255,0.8)"); 
+            $("nav ul li a").css("color","#000");
+            $(".nav-btn i").css("background","#000");
+            $(".nav-wrapper").css("background","rgb(255,255,255)"); 
+            $(".hover-eff").removeClass("hover-color");
+            document.getElementById('logo_black').src='assets/img/logoAP_black.png';
+        }
+    });
+});
+
