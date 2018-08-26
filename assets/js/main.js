a1a10ca4a8dde916203711c6bcfc64eea4cd4593
michalber <37382499+michalber@users.noreply.github.com>
2018-08-27 01:20:57 +0200

    Update4

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ $(function(){
 $(document).ready(function(){
     $(window).scroll(function() { 
         if ($(document).scrollTop() > 100) { 
-            $("nav").css("background", "rgb(125, 106, 96,0.8)");
+            $("nav").css("background", "rgb(125, 106, 96,0.8)");           
             $("nav ul li a").css("color","#fff");
             $(".nav-btn i").css("background","#fff");
             $(".nav-wrapper").css("background","rgb(125, 106, 96)");
@@ -16,7 +16,7 @@ $(document).ready(function(){
             document.getElementById('logo_black').src='assets/img/logoAP_white.png';
         } 
         else {
-            $("nav").css("background", "rgba(255, 255, 255,0.8)"); 
+            $("nav").css("background", "rgba(255, 255, 255,0.8)");             
             $("nav ul li a").css("color","#000");
             $(".nav-btn i").css("background","#000");
             $(".nav-wrapper").css("background","rgb(255,255,255)"); 
