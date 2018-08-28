de5c53cc99ba2f7c377ca0c16a013d59b8e38061
michalber <37382499+michalber@users.noreply.github.com>
2018-08-28 23:41:29 +0200

    Update 5.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,19 +9,19 @@ $(document).ready(function(){
     $(window).scroll(function() { 
         if ($(document).scrollTop() > 100) { 
 //            $("nav").css("background", "rgb(125, 106, 96,0.8)");           
-            $("nav").css("background-image", "linear-gradient( 175.9deg,rgba(125, 106, 96, 0.9) 15%, rgba(85,76,74,0.9) 87.6%)");
+            $("nav").css("background", "linear-gradient(175.9deg,rgba(125, 106, 96, 0.9) 15%, rgba(95, 71, 61, 0.9) 87.6%)");
             $("nav ul li a").css("color","#fff");
             $(".nav-btn i").css("background","#fff");
-            $(".nav-wrapper").css("background-image","linear-gradient( 175.9deg,rgba(125, 106, 96, 0.9) 15%, rgba(85,76,74,0.9) 87.6%)");
+            $(".nav-wrapper").css("background-image","linear-gradient( 175.9deg,rgba(125, 106, 96, 1    ) 15%, rgba(95, 71, 61, 1) 87.6%)");
             $(".hover-eff").addClass("hover-color");
             document.getElementById('logo_black').src='assets/img/logoAP_white.png';
         } 
         else {
           //  $("nav").css("background", "rgba(255, 255, 255,0.8)");             
-            $("nav").css("background-image", "linear-gradient(to right, rgba(232, 241, 255, 0.7), rgba(219, 230, 245, 0.7))");
+            $("nav").css("background", "linear-gradient(to right, hsla(217, 100%, 97%, 0.7), hsla(215, 57%, 97%, 0.7))");
             $("nav ul li a").css("color","#000");
             $(".nav-btn i").css("background","#000");
-            $(".nav-wrapper").css("background-image","linear-gradient(to right, rgba(232, 241, 255, 0.7), rgba(219, 230, 245, 0.7))"); 
+            $(".nav-wrapper").css("background-image","linear-gradient(to right, hsla(217, 100%, 97%, 1), hsla(215, 57%, 97%, 1))"); 
             $(".hover-eff").removeClass("hover-color");
             document.getElementById('logo_black').src='assets/img/logoAP_black.png';
         }
